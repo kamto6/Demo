805974a2560a845c6c70100da927b86de4e7a936
jiekangwei <kwjie5566@163.com>
2020-11-13 18:36:35 +0800

    Initial Commit

---
+++ DEMO2/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  DEMO2
+//
+//  Created by 揭康伟 on 2020/11/13.
+//  Copyright © 2020 揭康伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ DEMO2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DEMO2
+//
+//  Created by 揭康伟 on 2020/11/13.
+//  Copyright © 2020 揭康伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
